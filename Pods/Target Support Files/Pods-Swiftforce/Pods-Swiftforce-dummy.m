53cab288a4223357dd8a830e4eccc1042be83f31
Igor Androsov <igor-androsov@bosshogs-MacBook-Pro-2.local>
2016-08-04 08:35:32 -0400

    Ignore pods

--- Pods/Target Support Files/Pods-Swiftforce/Pods-Swiftforce-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Swiftforce : NSObject
-@end
-@implementation PodsDummy_Pods_Swiftforce
-@end
--- Pods/Target Support Files/SwiftlySalesforce/SwiftlySalesforce-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SwiftlySalesforce.h"
-
-FOUNDATION_EXPORT double SwiftlySalesforceVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftlySalesforceVersionString[];
-
